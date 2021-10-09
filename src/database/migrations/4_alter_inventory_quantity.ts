be2d564089c213de7be57c46bfeb2c6fc819a104
Akul Agrawal <31360523+akulagrawal@users.noreply.github.com>
2021-10-09 21:38:35 +0530

    Update 4_alter_inventory_quantity.ts

--- src/database/migrations/4_alter_inventory_quantity.ts
+++ src/database/migrations/4_alter_inventory_quantity.ts
@@ -1,13 +1,9 @@
 import Knex, { SchemaBuilder } from 'knex';
 
 export async function up(knex: Knex): Promise<SchemaBuilder> {
-  return knex.schema.table('inventories', tableBuilder => {
-    tableBuilder.renameColumn('iventoryQuantity', 'inventoryQuantity');
-  });
+  return knex.schema.table('inventories', tableBuilder => tableBuilder);
 }
 
 export async function down(knex: Knex): Promise<SchemaBuilder> {
-  return knex.schema.table('inventories', tableBuilder => {
-    tableBuilder.renameColumn('inventoryQuantity', 'iventoryQuantity');
-  });
+  return knex.schema.table('inventories', tableBuilder => tableBuilder);
 }
